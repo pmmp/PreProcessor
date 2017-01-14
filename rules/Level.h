593a330dd9d56621a1912fd6d4aa832d7a5583ae
Dylan K. Taylor <odigiman@gmail.com>
2017-01-14 15:22:50 +0000

    Some updates for MCPE 1.0

--- rules/Level.h
+++ rules/Level.h
@@ -3,8 +3,8 @@
 
 #define $this->isChunkLoaded(chunkX, chunkZ) (isset($this->chunks[Level::chunkHash(chunkX, chunkZ)]) or $this->provider->isChunkLoaded(chunkX, chunkZ))
 
-#define $this->getBlockIdAt(x, y, z) ($this->getChunk(x >> 4, z >> 4, true)->getBlockId(x & 0x0f, y & 0x7f, z & 0x0f))
-#define $this->getBlockDataAt(x, y, z) ($this->getChunk(x >> 4, z >> 4, true)->getBlockData(x & 0x0f, y & 0x7f, z & 0x0f))
+#define $this->getBlockIdAt(x, y, z) ($this->getChunk(x >> 4, z >> 4, true)->getBlockId(x & 0x0f, y, z & 0x0f))
+#define $this->getBlockDataAt(x, y, z) ($this->getChunk(x >> 4, z >> 4, true)->getBlockData(x & 0x0f, y, z & 0x0f))
 
-#define $this->getBlockLightAt(x, y, z) ($this->getChunk(x >> 4, z >> 4, true)->getBlockLight(x & 0x0f, y & 0x7f, z & 0x0f))
-#define $this->getBlockSkyLightAt(x, y, z) ($this->getChunk(x >> 4, z >> 4, true)->getBlockSkyLight(x & 0x0f, y & 0x7f, z & 0x0f))
+#define $this->getBlockLightAt(x, y, z) ($this->getChunk(x >> 4, z >> 4, true)->getBlockLight(x & 0x0f, y, z & 0x0f))
+#define $this->getBlockSkyLightAt(x, y, z) ($this->getChunk(x >> 4, z >> 4, true)->getBlockSkyLight(x & 0x0f, y, z & 0x0f))
