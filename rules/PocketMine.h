86961f81970a49f8c25e18d0d414961d7142a84d
Dylan K. Taylor <odigiman@gmail.com>
2017-12-30 14:13:56 +0000

    Fixed some PHP constants getting expanded as C++ macros by CPP

--- rules/PocketMine.h
+++ rules/PocketMine.h
@@ -2,6 +2,8 @@
 #include <rules/Binary.h>
 
 #define COMPILE 1
+#undef __FILE__
+#undef __LINE__
 
 #define Level::chunkHash(chunkX, chunkZ) (((chunkX) & 0xFFFFFFFF) << 32) | ((chunkZ) & 0xFFFFFFFF)
 #define Level::blockHash(x, y, z) (((x) & 0xFFFFFFF) << 36) | (((y) & 0xff) << 28) | ((z) & 0xFFFFFFF)
