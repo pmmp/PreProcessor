4d4d2a74a6ab9c77480b1eacbf69d0e92e65e1c2
Dylan K. Taylor <odigiman@gmail.com>
2019-06-22 15:51:15 +0100

    updated blockhash

--- rules/PocketMine.h
+++ rules/PocketMine.h
@@ -6,6 +6,6 @@
 #undef __LINE__
 
 #define World::chunkHash(chunkX, chunkZ) (((chunkX) & 0xFFFFFFFF) << 32) | ((chunkZ) & 0xFFFFFFFF)
-#define World::blockHash(x, y, z) (((x) & 0xFFFFFFF) << 36) | (((y) & 0xff) << 28) | ((z) & 0xFFFFFFF)
+#define World::blockHash(x, y, z) (((x) & 0x7ffffff) << 37) | ((((y) - World::HALF_Y_MAX) & 0x3ff) << 27) | ((z) & 0x7ffffff)
 #define World::getXZ(hash, chunkX, chunkZ) chunkX = (hash >> 32); chunkZ = (hash & 0xFFFFFFFF) << 32 >> 32
-#define World::getBlockXYZ(hash, X, Y, Z) X = (hash >> 36); Y = ((hash >> 28) & 0xff); Z = (hash & 0xFFFFFFF) << 36 >> 36
+#define World::getBlockXYZ(hash, X, Y, Z) X = (hash) >> 37; Y = ((hash) << 27 >> 54) + World::HALF_Y_MAX; Z = (hash) << 37 >> 37
