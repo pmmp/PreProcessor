c8c0557ddf159413352de2a74a013d40fbfe3a69
Dylan K. Taylor <odigiman@gmail.com>
2020-10-17 16:01:41 +0100

    Updated preprocessor definitions for morton2d chunkhash migration

--- rules/PocketMine.h
+++ rules/PocketMine.h
@@ -5,7 +5,7 @@
 #undef __FILE__
 #undef __LINE__
 
-#define World::chunkHash(chunkX, chunkZ) (((chunkX) & 0xFFFFFFFF) << 32) | ((chunkZ) & 0xFFFFFFFF)
+#define World::chunkHash(chunkX, chunkZ) morton2d_encode((chunkX), (chunkZ))
 #define World::blockHash(x, y, z) (((x) & 0x7ffffff) << 37) | ((((y) - World::HALF_Y_MAX) & 0x3ff) << 27) | ((z) & 0x7ffffff)
-#define World::getXZ(hash, chunkX, chunkZ) chunkX = (hash >> 32); chunkZ = (hash & 0xFFFFFFFF) << 32 >> 32
+#define World::getXZ(hash, chunkX, chunkZ) [chunkX, chunkZ] = morton2d_decode(hash)
 #define World::getBlockXYZ(hash, X, Y, Z) X = (hash) >> 37; Y = ((hash) << 27 >> 54) + World::HALF_Y_MAX; Z = (hash) << 37 >> 37
