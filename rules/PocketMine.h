8a3163aad6bb8683a11d78fb907d2f43b0b0fc0a
Dylan T <odigiman@gmail.com>
2021-04-07 20:03:47 +0100

    Remove definitions for blockHash
    this is complex enough now that it can't be inlined.

--- rules/PocketMine.h
+++ rules/PocketMine.h
@@ -6,6 +6,4 @@
 #undef __LINE__
 
 #define World::chunkHash(chunkX, chunkZ) morton2d_encode((chunkX), (chunkZ))
-#define World::blockHash(x, y, z) (((x) & 0x7ffffff) << 37) | ((((y) - World::HALF_Y_MAX) & 0x3ff) << 27) | ((z) & 0x7ffffff)
 #define World::getXZ(hash, chunkX, chunkZ) [chunkX, chunkZ] = morton2d_decode(hash)
-#define World::getBlockXYZ(hash, X, Y, Z) X = (hash) >> 37; Y = ((hash) << 27 >> 54) + World::HALF_Y_MAX; Z = (hash) << 37 >> 37
